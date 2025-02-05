91f928acd47b2fccc61ddec4b343bcc3f4afc618
harun <harun@HARRRRRUUUUNNNN>
2025-02-05 03:41:11 +0300

    Add project files.

---
+++ hesapmakinesi/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class program {
+    static void
+          Main(string[] args)
+    {
+        Console.Title = " My C# Calculator";
+        Console.ForegroundColor = ConsoleColor.Green;
+        int num1;
+        int num2;
+        int result; 
+        string operations;
+        Console.WriteLine("Enter the first number");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the Second number");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the operations (+,-,*,/)");
+        operations = Console.ReadLine();
+
+        switch (operations)
+        {
+            case ("+"):
+                result = num1 + num2;
+                Console.WriteLine("The answer is " + result);
+                break;
+        }
+        switch (operations)
+        {
+            case ("-"):
+                result = num1 - num2;
+                Console.WriteLine("The answer is " + result);
+                break;
+        }
+        switch (operations)
+        {
+            case ("*"):
+                result = num1 * num2;
+                Console.WriteLine("The answer is " + result);
+                break;
+        }
+        switch (operations)
+        {
+            case ("/"):
+                result = num1 / num2;
+                Console.WriteLine("The answer is " + result);
+                break;
+        }
+        Console.ReadKey();
+    }
+    
+}
